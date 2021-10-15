c5dc766f131a3a8ca4d39785439c45ea83be3e52
scotttyso <tyscott@cisco.com>
2021-10-15 00:46:40 -0400

    adding output

--- modules/tfe/output.tf
+++ modules/tfe/output.tf
@@ -3,7 +3,7 @@
 # Output Agent Pool ID from Terraform Cloud
 #__________________________________________________________
 
-# output "tfc_agent_pool" {
-#   description = "Terraform Cloud Agent Pool ID."
-#   value       = module.tfc_agent_pool
-# }
+output "tfc_agent_pool" {
+  description = "Terraform Cloud Agent Pool ID."
+  value       = module.tfc_agent_pool
+}
